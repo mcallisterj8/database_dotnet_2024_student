2fbb251a4ed9a85b133887a4dabd56a567465334
Jacob McAllister <mcallisterj@westerntc.edu>
2024-05-29 11:07:58 -0500

    In class push
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- ECommercePlatform/Models/Order.cs
+++ ECommercePlatform/Models/Order.cs
@@ -1,1 +1,22 @@
-public class Order {}
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommercePlatform.Models;
+
+public class Order {
+    [Key]
+    public int Id { get; set;}
+
+    [Required]
+    public DateTime OrderDate { get; set; }
+
+    [Required]
+    public decimal TotalAmount { get; set; }
+
+    [ForeignKey("Customer")]
+    public int CustomerId { get; set; }
+    public virtual Customer? Customer { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
