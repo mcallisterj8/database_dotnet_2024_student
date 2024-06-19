018f801a2065f95b1a0b625b97d3d9b2bf846d38
Jacob McAllister <mcallisterj@westerntc.edu>
2024-06-19 10:21:02 -0500

    In class update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ VanillaMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VanillaMvcApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
